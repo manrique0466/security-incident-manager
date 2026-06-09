0058d90e2d55ab3dfe1191453a7adc6f510b0ec1
martinez0466 <camilo93.martinez@gmail.com>
2026-06-09 11:53:38 +0200

    chore: initial project setup

---
+++ src/main/java/com/securityincidentmanager/IncidentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.securityincidentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securityincidentmanager/IncidentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityincidentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
